3495b7383c6207fbd5c8c357be026cfada12e596
Noor Muhammad <connectwithnoor1@gmail.com>
2022-07-10 16:19:04 +0500

    FollowersCard Component

---
+++ src/components/FollowersCard/FollowersCard.jsx
@@ -0,0 +1,32 @@
+import { Followers } from '../../Data/FollowersData';
+
+import './FollowersCard.css';
+
+const FollowersCard = () => {
+  return (
+    <div className='followersCard'>
+      <h3>Who is following you</h3>
+
+      {Followers.map((follower) => {
+        return (
+          <div className='follower' key={follower.id}>
+            <div>
+              <img
+                src={follower.img}
+                alt='follower-img'
+                className='followerImg'
+              />
+              <div className='name'>
+                <span>{follower.name}</span>
+                <span>@{follower.username}</span>
+              </div>
+            </div>
+            <button className='button fc-button'>Follow</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default FollowersCard;
