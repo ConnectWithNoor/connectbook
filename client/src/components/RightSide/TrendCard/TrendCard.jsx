6ebcde2cee87d8c45e70b92a8b5db1ca34f4b055
Noor Muhammad <connectwithnoor1@gmail.com>
2022-08-22 23:37:27 +0500

    remove import statement

--- client/src/components/RightSide/TrendCard/TrendCard.jsx
+++ client/src/components/RightSide/TrendCard/TrendCard.jsx
@@ -1,6 +1,37 @@
-import TrendData from '../../../data/TrendsData';
-
 import './TrendCard.css';
+
+const TrendData = [
+  {
+    id: 0,
+    name: 'SmackDown',
+    shares: 97,
+  },
+  {
+    id: 1,
+    name: 'Avangers',
+    shares: 80.5,
+  },
+  {
+    id: 2,
+    name: 'Lewandowski',
+    shares: 75.5,
+  },
+  {
+    id: 3,
+    name: 'Reactjs',
+    shares: 72,
+  },
+  {
+    id: 4,
+    name: 'Manchin',
+    shares: 71.9,
+  },
+  {
+    id: 5,
+    name: 'Happy Mondays',
+    shares: 20,
+  },
+];
 
 const TrendCard = () => {
   return (
